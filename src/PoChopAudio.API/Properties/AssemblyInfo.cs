479ff659cea14b2a81908840a56215a8f5a54517
punkouter26 <punkouter26@gmail.com>
2026-08-24 22:33:05 -0400

    ...

--- src/PoChopAudio.API/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("PoChopAudio.Unit")]
-[assembly: InternalsVisibleTo("PoChopAudio.Integration")]
-[assembly: InternalsVisibleTo("PoChopAudio.E2EAPI")]
